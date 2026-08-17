c2dd257b5be406a2bb33ce7c1dd3c2eeddcce1d6
funtimefunnyy1-droid <funtimefunnyy1@gmail.com>
2026-08-17 17:25:16 +0530

    Add StreamWithState to StreamItem creation

--- app/src/main/java/org/schabi/newpipe/local/feed/FeedFragment.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/FeedFragment.kt
@@ -59,6 +59,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import org.schabi.newpipe.NewPipeDatabase
 import org.schabi.newpipe.R
 import org.schabi.newpipe.database.feed.model.FeedGroupEntity
+import org.schabi.newpipe.database.feed.model.StreamWithState
 import org.schabi.newpipe.database.subscription.SubscriptionEntity
 import org.schabi.newpipe.databinding.FragmentFeedBinding
 import org.schabi.newpipe.databinding.PlaylistControlBinding
@@ -252,8 +253,12 @@ class FeedFragment : BaseStateFragment<FeedState>() {
                 } else {
                     feedBinding.continueWatchingContainer.isVisible = true
                     val carouselItems = entries.take(10).map { entry ->
+                        val streamWithState = StreamWithState().apply {
+                            stream = entry.streamEntity
+                            state = null
+                        }
                         StreamItem(
-                            entry.streamEntity.toStreamInfoItem()
+                            streamWithState
                         ).apply {
                             itemVersion = StreamItem.ItemVersion.CARD
                         }
